5c85ffa89aac584e8734ce4ec59cd7d67cb36bfe
Your Name <you@example.com>
2023-08-05 23:14:08 +0500

    second commit

--- lib/tipcalculator.dart
+++ lib/tipcalculator.dart
@@ -4,67 +4,102 @@ class TipCalculator extends StatefulWidget {
   const TipCalculator({super.key});
 
   @override
-  State<TipCalculator> createState() => _TipCalculatorState();
+  // ignore: library_private_types_in_public_api
+  _TipCalculatorState createState() => _TipCalculatorState();
 }
 
 class _TipCalculatorState extends State<TipCalculator> {
-  double totalTip = 0;
-  double totalBill = 0.0;
-  TextEditingController billController = TextEditingController();
-  TextEditingController tipController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  double _billAmount = 0.0;
+  double _tipPercentage = 15.0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Tip Calculator")),
-        body: Form(
+      appBar: AppBar(
+        title: const Text('Tip Calculator'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
           key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 12),
-            child: Column(children: [
-              Text("Total Tip: $totalTip"),
-              Text("Total Bill after calculating Tip: $totalBill"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
               TextFormField(
-                controller: billController,
                 keyboardType:
                     const TextInputType.numberWithOptions(decimal: true),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return ('Bill Amount should not be Empty');
-                  } else {
-                    return null;
-                  }
-                },
-                decoration:
-                    const InputDecoration(hintText: "Enter Bill Amount"),
-              ),
-              TextFormField(
-                controller: tipController,
+                decoration: const InputDecoration(
+                  labelText: 'Bill Amount',
+                ),
                 validator: (value) {
                   if (value == '') {
-                    return 'Tip Amount should not be empty';
-                  } else {
-                    return null;
+                    return 'Please enter the bill amount';
                   }
+                  if (double.tryParse(value!) == null) {
+                    return 'Please enter a valid number';
+                  }
+                  return null;
                 },
-                decoration:
-                    const InputDecoration(hintText: "Enter Tip Percentage"),
+                onSaved: (value) {
+                  _billAmount = double.parse(value!);
+                },
               ),
+              const SizedBox(height: 16),
+              TextFormField(
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+                decoration: const InputDecoration(
+                  labelText: 'Tip Percentage',
+                ),
+                validator: (value) {
+                  if (value == '') {
+                    return 'Please enter the tip percentage';
+                  }
+                  if (double.tryParse(value!) == null ||
+                      double.parse(value) <= 0 ||
+                      double.parse(value) > 100) {
+                    return 'Please enter a valid percentage between 0 and 100';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _tipPercentage = double.parse(value!);
+                },
+              ),
+              const SizedBox(height: 16),
               ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      double billValue =
-                          double.tryParse(billController.text) ?? 0.0;
-                      double tipValue =
-                          double.tryParse(tipController.text) ?? 0.0;
-                      totalTip = billValue * (tipValue / 100);
-                      totalBill = billValue + totalTip;
-                    });
-                  },
-                  child: const Text("Calculate Tip"))
-            ]),
+                onPressed: _calculateTotal,
+                child: const Text('Calculate'),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  void _calculateTotal() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      double tipAmount = _billAmount * (_tipPercentage / 100);
+      double totalAmount = _billAmount + tipAmount;
+
+      // Display the result (you can show it in a dialog or a new screen)
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Total Amount'),
+          content: Text(
+              'Total amount to be paid: \$${totalAmount.toStringAsFixed(2)}'),
+          actions: [
+            ElevatedButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
   }
 }
